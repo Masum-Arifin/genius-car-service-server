96cf97f1245db2ff5e480c76e19a423016bea1db
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-04 21:40:46 +0600

    new code

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("services");
+    const orderCollection = client.db("geniusCar").collection("order");
 
     // all services get
     app.get("/services", async (req, res) => {
@@ -51,6 +52,12 @@ async function run() {
       const id = req.params.id
       const query = {_id: ObjectId(id)}
       const result = await serviceCollection.deleteOne(query)
+      res.send(result)
+    })
+    // order collection api
+    app.post('/order', async (req, res) =>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order)
       res.send(result)
     })
   } 
